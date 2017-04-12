7795c77a41f9c1a80c06ed61cd74013989e12723
Erik Johansson <clearhythm@hotmail.com>
2017-04-12 13:55:04 -0700

    first project - magic cubes

---
+++ ErikBurns_Project1/Assets/UdacityVR/Scripts/UdacityFunEffects.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UdacityFunEffects : MonoBehaviour {
+
+    public const float SUN_ROTATION_SPEED = 40f;
+    private Vector3 SUN_UP = new Vector3(70f, 0f, 0f); //vectors cannot be consts :(
+    private Vector3 SUN_DOWN = new Vector3(220f, 0f, 0f);
+
+    public Light sunLight;
+    public Light flashLight;
+
+    public int numCubes = 10;
+    public GameObject cubePrefab;
+
+    public Teleport cube;
+    public Teleport sphere;
+
+    private GameObject activeShape;
+    private bool isSunUp = true;
+
+    public GameObject[] cubes;
+
+    // Use this for initialization
+    void Start () {
+        cubes = new GameObject[numCubes];
+
+        if (cube.gameObject.activeInHierarchy)
+            activeShape = cube.gameObject;
+        else
+            activeShape = sphere.gameObject;
+	}
+
+
+    public void ChangeParticleColor()
+    {
+        Color c = Random.ColorHSV(0f, 1f, 0f, 1f, 0f, 1f, 1f, 1f);
+        cube.GetComponent<ParticleSystem>().startColor = c;
+        sphere.GetComponent<ParticleSystem>().startColor = c;
+    }
+
+    public void ToggleSun()
+    {
+        StopAllCoroutines();
+        isSunUp = !isSunUp;
+        StartCoroutine(AnimateSun(isSunUp));
+    }
+
+    public void ToggleFlashLight()
+    {
+        flashLight.gameObject.SetActive(!flashLight.gameObject.activeInHierarchy);
+    }
+
+    public void ToggleShape()
+    {
+        if (cube.gameObject.activeInHierarchy)
+        {
+            Vector3 cubePosition = cube.transform.position;
+            cube.gameObject.SetActive(false);
+            sphere.gameObject.SetActive(true);
+            sphere.transform.position = cubePosition;
+
+            activeShape = sphere.gameObject;
+        } else
+        {
+            Vector3 spherePosition = sphere.transform.position;
+            cube.gameObject.SetActive(true);
+            sphere.gameObject.SetActive(false);
+            cube.transform.position = spherePosition;
+
+            activeShape = cube.gameObject;
+        }
+    }
+
+    public void SpawnRandomCubes()
+    {
+        StopCoroutine("SpawnCubesWithDelay");
+        StartCoroutine("SpawnCubesWithDelay");
+    }
+
+    private IEnumerator SpawnCubesWithDelay()
+    {
+        for (int i = 0; i < cubes.Length; i++)
+        {
+            Destroy(cubes[i]);
+            cubes[i] = Instantiate(cubePrefab);
+            cubes[i].GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 0f, 1f, 0f, 1f, 1f, 1f);
+            cubes[i].transform.position = new Vector3(Random.Range(-2f, 2f), 25f, Random.Range(-2f, 2f));
+            Rigidbody r = cubes[i].GetComponent<Rigidbody>();
+            r.angularVelocity = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+
+            yield return new WaitForSeconds(.1f);
+        }
+    }
+
+    private IEnumerator AnimateSun(bool isSunUp)
+    {
+        Quaternion target = Quaternion.Euler(isSunUp ? SUN_UP : SUN_DOWN);
+        float startAngle = Quaternion.Angle(sunLight.transform.rotation, target);
+        while (true)
+        {
+            float currAngle = Quaternion.Angle(sunLight.transform.rotation, target);
+            if (currAngle > 0.5f) {
+                sunLight.transform.Rotate(new Vector3(SUN_ROTATION_SPEED * Time.deltaTime, 0f, 0f));
+                float intensity = currAngle / startAngle;
+                if (isSunUp)
+                    intensity = 1f - intensity;
+
+                sunLight.intensity = intensity;
+
+                yield return new WaitForEndOfFrame();
+            } else {
+                break;
+            }
+        }
+    }
+}
---
+++ ErikBurns_Project1/README.md
@@ -0,0 +1,12 @@
+GVR_SDK_VERSION = "1.0";
+
+= What I Liked =
+I liked the simplicity of this project, and a quick start with some VR candy!
+The project was a good example of the publishing workflow and also how to submit
+a project for review by Udacity.
+
+= What I found Challenging =
+Navigating the scene view within Unity without a mouse (using Macbook trackpad)
+is a bit challenging. Moving forward, I may use my external mouse while in Unity.
+Also, it was hard to find which version of the Google VR SDK I was using. I had
+to Google for a bit to figure it out, and finally asked my Udacity mentor.
